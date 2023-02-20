68bfa19047c5ec663b3a2fc20700e5f9beb5d21c
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-02-20 19:33:33 +0700

    SiGG-SatNet-Queries-Series-Configuration-Find_SQL_Server_service_info.sql

---
+++ Find_SQL_Server_service_info.sql
@@ -0,0 +1,16 @@
+SELECT servicename,
+       startup_type_desc,
+       status_desc,
+       last_startup_time,
+       service_account,
+       is_clustered,
+       cluster_nodename,
+       instant_file_initialization_enabled
+FROM sys.dm_server_services;
+
+SELECT registry_key,
+       value_name,
+       value_data
+FROM sys.dm_server_registry
+WHERE value_name LIKE '%Port%'
+      AND registry_key LIKE '%IPAll';
