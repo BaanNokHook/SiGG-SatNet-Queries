0cfd1477398690f57b7833e896c7856f0d50df9a
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-03-04 10:45:11 +0700

    SiGG-SatNet-Queries-Series-Performance-sp_WhoIsActive answer specific questions.sql

---
+++ sp_WhoIsActive answer specific questions.sql
@@ -0,0 +1,22 @@
+EXEC sp_WhoIsActive @output_column_list = '[dd%][session_id][%delta][login_name][sql_text][%]',
+                    @delta_interval = 5;
+
+-- What filled up the transaction log?
+EXEC sp_WhoIsActive @output_column_list = '[dd%][session_id][tran%][login_name][sql_text][%]',
+                    @get_transaction_info = 1;
+
+-- What's using the most memory?
+EXEC sp_WhoIsActive @output_column_list = '[dd%][session_id][%memory%][login_name][sql_text][%]',
+                    @get_memory_info = 1;
+
+-- What filled up tempdb?
+EXEC sp_WhoIsActive @output_column_list = '[start_time][session_id][temp%][sql_text][query_plan][wait_info][%]',
+                    @get_plans = 1,
+                    @sort_order = '[tempdb_current] DESC';
+
+-- Is there blocking?
+EXEC sp_WhoIsActive @output_column_list = '[start_time][session_id][block%][login%][locks][sql_text][%]',
+                    @find_block_leaders = 1,
+                    @get_locks = 1,
+                    @get_additional_info = 1,
+                    @sort_order = '[blocked_session_count] DESC';
