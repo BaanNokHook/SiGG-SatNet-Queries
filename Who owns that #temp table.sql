4f4cba4ce98af1f5a6a844bc6ee9463994c03329
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-03-04 17:25:47 +0700

    SiGG-SatNet-Queries-Series-TempDB-Caldon-Check-Script

---
+++ Who owns that #temp table.sql
@@ -0,0 +1,23 @@
+DECLARE @filename VARCHAR(MAX);
+ 
+SELECT  @filename = SUBSTRING([path], 0,
+                              LEN([path]) - CHARINDEX('\', REVERSE([path]))
+                              + 1) + '\Log.trc'
+FROM    sys.traces
+WHERE   is_default = 1;  
+ 
+SELECT  o.name,
+        o.[object_id],
+        o.create_date,
+        gt.SPID,
+        NTUserName = gt.NTDomainName + '\' + gt.NTUserName,
+        SQLLogin = gt.LoginName,
+        gt.HostName,
+        gt.ApplicationName,
+        gt.TextData -- don't bother, always NULL 
+FROM    sys.fn_trace_gettable(@filename, DEFAULT) AS gt
+        INNER JOIN tempdb.sys.objects AS o ON gt.ObjectID = o.[object_id]
+WHERE   gt.DatabaseID = 2
+        AND gt.EventClass = 46 -- (Object:Created Event from sys.trace_events)  
+        AND gt.EventSubClass = 1 -- Commit
+        AND o.name LIKE N'#%';
